e11af3ef9b1f9ca287ee7589f22792716b4eaa03
eballo <enric.ballo@travelperk.com>
2020-03-20 16:16:46 +0100

    add tests patch

--- app/recipe/tests/test_serializer_recipe.py
+++ app/recipe/tests/test_serializer_recipe.py
@@ -1,20 +1,16 @@
 from django.test import TestCase
-
 from rest_framework import status
 from rest_framework.test import APIClient
-
 from recipe.models import Recipe
-
 from recipe.serializers import RecipeSerializer
 
-RECIPE_URL = "/api/recipes/"
-
+RECIPE_URL = "/api/recipies/"
 
 def sample_recipe(**params):
     """Create and return a sample recipe for testing"""
     defaults = {
-        'name': 'Sample recipe name',
-        'description': 'Sample recipe description'
+        "name": "Pizza",
+        "description": "Put it in the oven"
     }
     defaults.update(params)
 
@@ -22,12 +18,31 @@ def sample_recipe(**params):
 
 
 class PublicRecipeApiTests(TestCase):
+    """ Public recipe API """
 
     def setUp(self):
         self.client = APIClient()
 
+    def test_create_a_recipe(self):
+        """Test creating a recipes POST"""
+        
+        payload = {
+                "name": "Pizza",
+                "description": "Put it in the oven",
+                "ingredients": [
+                                {"name": "dough"}, 
+                                {"name": "cheese"},
+                                {"name": "tomato"}
+                                ]
+                }
+        
+        res = self.client.post(RECIPE_URL, payload)
+        #import pdb; pdb.set_trace()
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+
     def test_retrieve_one_recipe(self):
-        """Test retriving a list of recipes"""
+        """Test retriving a list of recipes GET"""
         sample_recipe()
 
         res = self.client.get(RECIPE_URL)
@@ -39,14 +54,32 @@ class PublicRecipeApiTests(TestCase):
         self.assertEqual(res.data, serilizer.data)
 
     def test_retrieve_multiple_recipes(self):
-        """Test retriving a list of recipes"""
+        """Test retriving a list of recipes GET"""
         sample_recipe()
         sample_recipe()
 
         res = self.client.get(RECIPE_URL)
 
         recipes = Recipe.objects.all()
-        serilizer = RecipeSerializer(recipes, many=True)
+        serializer = RecipeSerializer(recipes, many=True)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertEqual(res.data, serilizer.data)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_update_recipe(self):
+        """Test update a recipe PATCH"""
+        recipe = sample_recipe()
+        
+        payload = {
+            "name": "Pizza",
+            "description": "Put it in the oven",
+            "ingredients": [
+                            {"name": "dough"}, 
+                            {"name": "cheese"},
+                            {"name": "tomato"}
+                            ]
+            }
+
+        res = self.client.patch(RECIPE_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
