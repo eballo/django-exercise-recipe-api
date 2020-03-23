53d92a91e6fc1649e1a30d385156dca41c86e7a9
eballo <enric.ballo@travelperk.com>
2020-03-23 11:21:18 +0100

    update ref documentation

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -3,6 +3,9 @@ from rest_framework import viewsets
 from recipe.serializers import RecipeSerializer
 from recipe.models import Recipe
 
+# Ref Documentation:
+# https://www.django-rest-framework.org/api-guide/filtering/
+
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """ Manage a Recipe in the database """
